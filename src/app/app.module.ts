0954963905181d3c85af4f7c9e48fc4b15723c1f
jmsebastian <sebastianjmt@hotmail.com>
2019-04-20 19:08:10 -0400

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RegistrarEmpleadoComponent } from './registrar-empleado/registrar-empleado.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrarEmpleadoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
